d1de1ead733e290afe7869483616230d62575377
Ioan Lazar <ilazar@Ioans-MacBook-Pro.local>
2020-09-20 16:22:43 +0300

    01

---
+++ README.md
@@ -0,0 +1,7 @@
+Change log
+
+01 Initial project
+
+- Ionic React quickstart, https://ionicframework.com/docs/react/quickstart
+- App, router, pages, components
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,13 +8,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>My App</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">My App</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
